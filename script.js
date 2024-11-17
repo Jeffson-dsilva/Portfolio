c91f92243516be796702e24338377ba4120329fb
Jeffson Preetham Dsilva <167344842+Jeffson-dsilva@users.noreply.github.com>
2024-11-17 23:00:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-// Smooth scroll for links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
+});
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+  document.querySelector('.navbar').classList.toggle('active');
+});
+
+
+
+window.addEventListener("scroll", function () {
+  const aboutSection = document.getElementById('about');
+  if (aboutSection.getBoundingClientRect().top <= window.innerHeight * 0.8) {
+    aboutSection.classList.add('active');
+  }
+
+  const projectsSection = document.getElementById('projects');
+  if (projectsSection.getBoundingClientRect().top <= window.innerHeight * 0.8) {
+    projectsSection.classList.add('active');
+  }
+
+  const contactSection = document.getElementById('contact');
+  if (contactSection.getBoundingClientRect().top <= window.innerHeight * 0.8) {
+    contactSection.classList.add('active');
+  }
+});
+
+
+
+const buttons = document.querySelectorAll('.about-btn');
+const aboutDetails = document.querySelectorAll('.about-details');
+
+
+
+function changeContent(event) {
+  buttons.forEach(btn => btn.classList.remove('active'));
+  aboutDetails.forEach(detail => detail.classList.remove('active'));
+
+  const contentId = event.target.getAttribute('data-content');
+  document.getElementById(contentId).classList.add('active');
+  event.target.classList.add('active');
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', changeContent);
+});
+
+
